ef1c77b6309492af889b04019e82370d7c606060
PavloShmyhelskiy <90800504+PavloShmyhelskiy@users.noreply.github.com>
2021-09-17 17:33:17 +0300

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -8,19 +8,18 @@ function padWithZero(number) {
 function showTime(){
     const now = new Date();
 
-    const dayOfWeek = WEEK_DAYS[now.getDay()]
-    const day = now.getDate()
-
-    const month = MONTHS[now.getMonth()]
-
+    const dayOfWeek = WEEK_DAYS[now.getDay()];
+    const day = now.getDate();
+    const seconds = now.getSeconds();
+    const month = MONTHS[now.getMonth()];
     const hours = padWithZero(now.getHours()); 
     const minutes = padWithZero(now.getMinutes()); 
 
     const time = `${dayOfWeek} ${day} ${month} ${hours}:${minutes}`;
     
     document.getElementById("clock").innerText = time;
-    
-    setTimeout(showTime, 1000); 
+    const restTime = (60-seconds)*1000;
+    setTimeout(showTime, restTime); 
 }
  
 showTime();
