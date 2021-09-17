810c7e7e2b3b5bee5ebf5713a805dc330bf72fae
PavloShmyhelskiy <90800504+PavloShmyhelskiy@users.noreply.github.com>
2021-09-17 17:33:39 +0300

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,11 +1,11 @@
-const settings_window = document.getElementById("settings_window");
+const settingsWindow = document.getElementById("settingsWindow");
 
-const settings_icon = document.getElementById("settings-icon");
+const settingsIcon = document.getElementById("settingsIcon");
 
-settings_icon.addEventListener('click', (event) => {
-    if(settings_window.style['display'] == 'none'){
-        settings_window.style['display'] = 'block'
+settingsIcon.addEventListener('click', (event) => {
+    if(settingsWindow.style.display == 'none'){
+        settingsWindow.style.display = 'block'
     } else {
-        settings_window.style['display'] = 'none'
+        settingsWindow.style.display = 'none'
     }
     })
